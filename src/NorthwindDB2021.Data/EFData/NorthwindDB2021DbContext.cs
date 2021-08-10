cf75f0581b788412ff2f75b6b8f1a79f9178a6b1
samuelp101 <samuelphung@live.com>
2021-08-10 15:10:47 -0700

    MVC project added

---
+++ src/NorthwindDB2021.Data/EFData/NorthwindDB2021DbContext.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindDB2021.Models;
+
+#nullable disable
+
+namespace NorthwindDB2021.Data.EFData
+{
+    public partial class NorthwindDB2021DbContext : DbContext
+    {
+        public NorthwindDB2021DbContext()
+        {
+        }
+
+        public NorthwindDB2021DbContext(DbContextOptions<NorthwindDB2021DbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=NorthwindDB2021");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.HasIndex(e => e.City, "City");
+
+                entity.HasIndex(e => e.CompanyName, "CompanyName");
+
+                entity.HasIndex(e => e.PostalCode, "PostalCode");
+
+                entity.HasIndex(e => e.Region, "Region");
+
+                entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+
+                entity.Property(e => e.Address).HasMaxLength(60);
+
+                entity.Property(e => e.City).HasMaxLength(15);
+
+                entity.Property(e => e.CompanyName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.ContactName).HasMaxLength(30);
+
+                entity.Property(e => e.ContactTitle).HasMaxLength(30);
+
+                entity.Property(e => e.Country).HasMaxLength(15);
+
+                entity.Property(e => e.PostalCode).HasMaxLength(10);
+
+                entity.Property(e => e.Region).HasMaxLength(15);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ src/NorthwindDB2021.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NorthwindDB2021.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
